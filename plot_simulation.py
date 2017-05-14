be656c3ad17200443b00889e86d03a877d5343ba
Sebastian Wilzbach <seb@wilzba.ch>
2017-05-14 03:57:50 +0200

    Add boxplot plotting

--- plot_simulation.py
+++ plot_simulation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from os import listdir
+from os import listdir, makedirs
 from os.path import isfile, join
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import time
+from datetime import datetime
 
 if len(sys.argv) < 2:
     print("No order file found")
@@ -37,6 +38,32 @@ for idx, file in enumerate(files):
     df_50_days = df_50_days.append(df)
 
 print("df generated.")
+day_image_folder = order_file.replace(".csv", "")
+makedirs(day_image_folder, exist_ok=True)
+
+for idx, file in enumerate(files):
+    fig, ax1 = plt.subplots(figsize=(20, 6))
+    day = df_daily[idx]
+    day_index = day.index.values
+    day_date = str(day_index[0].astype('M8[D]').astype('O'))
+    plt.xlim(day_index[0], day_index[-1])
+    ax1.plot(day.index.values, day.price.values)
+
+    date = day.index[0].date()
+    orders_all = orders.loc[str(date)].dropna()
+    orders_buy = orders_all.loc[orders_all.trades > 0]
+    orders_sale = orders_all.loc[orders_all.trades < 0]
+
+    width = 0.005
+    ax2 = ax1.twinx()
+    ax2.set_xlim(day_index[0], day_index[-1])
+    ax2.set_ylim(-105, 105)
+    ax2.bar(orders_buy.index.values, orders_buy.trades.values, width=width, color='g')
+    ax2.bar(orders_sale.index.values, orders_sale.trades.values, width=width, color='r');
+    plt.savefig(day_image_folder + "/" + day_date + ".svg", bbox_inches='tight')
+    plt.cla();
+    plt.clf();
+print("day image generated.")
 
 fig, ax_arr = plt.subplots(nrows=10, ncols=5, figsize=(30, 30))
 plt.subplots_adjust(hspace=0.5)
