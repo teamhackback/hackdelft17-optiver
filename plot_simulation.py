e59ea1b2ddc78f68c584ab99396d52469c6d6e22
Sebastian Wilzbach <seb@wilzba.ch>
2017-05-14 05:28:13 +0200

    use summative resample

--- plot_simulation.py
+++ plot_simulation.py
@@ -15,12 +15,9 @@ if len(sys.argv) < 2:
     exit(-1)
 
 order_file = sys.argv[1]
-binning_factor = 60
-items_per_day = 30600 / binning_factor
-days_per_tick = 5
 
 orders = pd.read_csv(order_file, parse_dates=['times'], index_col=['times'])
-orders = orders.resample('1T').mean()
+# orders = orders.resample('1T').mean()
 
 data_path = './optiver/data'
 print("starting parsing.")
@@ -33,13 +30,14 @@ df_50_days = pd.DataFrame()
 for idx, file in enumerate(files):
     df = pd.read_csv(join(data_path, file), parse_dates=['times'], index_col=['times'])
     # take an average over X seconds
-    df = df.resample('1T').mean()
+    # df = df.resample('1T').mean()
     df_daily.append(df)
     df_50_days = df_50_days.append(df)
 
 print("df generated.")
 day_image_folder = order_file.replace(".csv", "")
 makedirs(day_image_folder, exist_ok=True)
+print("starting generating days.")
 
 for idx, file in enumerate(files):
     fig, ax1 = plt.subplots(figsize=(20, 6))
@@ -51,16 +49,17 @@ for idx, file in enumerate(files):
 
     date = day.index[0].date()
     orders_all = orders.loc[str(date)].dropna()
-    orders_buy = orders_all.loc[orders_all.trades > 0]
-    orders_sale = orders_all.loc[orders_all.trades < 0]
+    orders_buy = orders_all.loc[orders_all.trades > 0].resample('1T').sum()
+    orders_sale = orders_all.loc[orders_all.trades < 0].resample('1T').sum()
 
     width = 0.0001
     ax2 = ax1.twinx()
     ax2.set_xlim(day_index[0], day_index[-1])
-    ax2.set_ylim(-105, 105)
+    # ax2.set_ylim(-105, 105)
     ax2.bar(orders_buy.index.values, orders_buy.trades.values, width=width, color='g')
     ax2.bar(orders_sale.index.values, orders_sale.trades.values, width=width, color='r')
     plt.savefig(day_image_folder + "/" + day_date + ".svg", bbox_inches='tight')
     plt.cla()
     plt.clf()
+    break
 print("day image generated.")
